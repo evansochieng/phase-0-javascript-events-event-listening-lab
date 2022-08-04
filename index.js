0de941468ab5ab52bb5e1913abb967d2d4d047ec
Evans Ochieng <evansochieng121@gmail.com>
2022-08-04 19:46:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    // Create the clickAlert function
+    function clickAlert(){
+        alert('I was clicked!');
+    }
+
+    //add listener
+    return input.addEventListener('click', clickAlert);
 }
+
+// Call the function
+addingEventListener();
+
+
+// Always remember to call the function
